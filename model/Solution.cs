f5c39342abe7557226f18b036296c87c6f17780a
MihrijeK <mihrije.kadriu@studentet.uni-pr.edu>
2022-05-07 15:17:20 +0200

    Solution and Techer Model

---
+++ model/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETT.model
+{
+    class Solution
+    {
+        private Random randomGenerator { get; set; }
+        private List<Assignment> assignments { get; set; }
+        private int cost { get; set; }
+
+        public Solution(List<Assignment> assignments, int set)
+        {
+            this.assignments = assignments;
+            this.cost = cost;
+        }
+
+    }
+}
---
+++ model/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETT.model
+{
+    class Teacher
+    {
+		private string id { get; set; }
+		private string name { get; set; }
+
+		public Teacher() { }
+
+		public Teacher(string id, string name)
+		{
+			this.id = id;
+			this.name = name;
+		}
+	}
+}
