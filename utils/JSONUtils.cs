b30541ee03576c5d2f0f2a34db714aca38197fa9
MihrijeK <42340737+MihrijeK@users.noreply.github.com>
2022-05-17 01:33:18 +0200

    Deserialize

--- utils/JSONUtils.cs
+++ utils/JSONUtils.cs
@@ -5,23 +5,23 @@ using Json.Net;
 using System.Collections.Generic;
 using AutoMapper;
 using System.Text.Json;
+using Newtonsoft.Json;
+using ETT.model;
 
 public class JSONUtils {
 
-	// public static <T> T convert(String json, IList<T> typeReference) {
-	// 	// 	ObjectMapper mapper = new ObjectMapper();
-	// 	// 	mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
-	// 	// 	mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true);
-	// 	// 	try {
-	// 	// 		return mapper.readValue(json, typeReference);
-	// 	// 	} catch (IOException e) {
-	// 	// 		 e.printStackTrace();
-	// 	// 	}
-	// 	// return null;
-	// 	return System.Text.Json.JsonSerializer.Deserialize<IList<T>>(json);
-	// }
 	
-	public static String convert(Object obj) {
+	public static Instance Convert(string json, Instance instance)
+	{
+		// Serialize Data.  
+		string data = JsonConvert.SerializeObject(json);
+		// Deserialize Data.  
+		Instance deserializedData  = JsonConvert.DeserializeObject<Instance>(data);
+
+		return deserializedData;
+
+	}
+	public static string convert(Object obj) {
 	
 		var opt = new JsonSerializerOptions(){ WriteIndented=true };
         string strJson = System.Text.Json.JsonSerializer.Serialize(obj, opt);
